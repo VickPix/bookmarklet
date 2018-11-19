c2130aa4e14721fb7ccca4b58b000254d082bc4e
Vincenzo Cimino <vicio.cimino@gmail.com>
2018-11-19 22:29:45 +0100

    Update denaro-lettera.js

--- denaro-lettera.js
+++ denaro-lettera.js
@@ -23,9 +23,9 @@ var fillVars = function(){
 	var denaro = document.querySelectorAll('td:nth-child(3)')
 	var lettera = document.querySelectorAll('td:nth-child(6)')
 	for (var i = 0; i < denaro.length; i++) {
-		console.log(parseInt(denaro[i].innerText.replace('.','')),parseInt(lettera[i].innerText.replace('.','')))
-		 sumDenaro += isNaN(parseInt(denaro[i].innerText.replace('.',''))) ? 0 : parseInt(denaro[i].innerText.replace('.',''));
-		 sumLettera += isNaN(parseInt(lettera[i].innerText.replace('.',''))) ? 0 : parseInt(lettera[i].innerText.replace('.',''));
+		console.log(parseInt(denaro[i].innerText.replace('.','')),parseInt(lettera[i].innerText.replace('.','')));
+		sumDenaro += isNaN(parseInt(denaro[i].innerText.replace('.',''))) ? 0 : parseInt(denaro[i].innerText.replace('.',''));
+		sumLettera += isNaN(parseInt(lettera[i].innerText.replace('.',''))) ? 0 : parseInt(lettera[i].innerText.replace('.',''));
 	}
   label.innerHTML = (sumLettera-sumDenaro).toLocaleString();
   if(sumLettera-sumDenaro > 0){
